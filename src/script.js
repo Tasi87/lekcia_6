20cb9036dd84c96b695852c8f00d93a1fc4462fd
Tasi <tasi87@gmail.com>
2026-03-10 14:06:34 +0100

    Asynchronny JS

--- src/script.js
+++ src/script.js
@@ -1,38 +1,12 @@
-//idea	Memory leak a prax
+//idea	Asynchronný JavaScript a JavaScript runtime
+//	-	Asynchrónny JavaScript umožňuje vykonávať dlhšie operácie na pozadí bez toho, aby sa zastavilo vykonávanie zvyšného kódu.
 
-	//note	1.	Globálne premenné
-	//!	-	písať len ak je to nevyhnutné, kvôli memory leaku
+// kód
+// asynchronný kód
+// kód
 
-	let a = 10; // globálna premenná
-	//bug		a = 10;	takto sa zvykne naznačovať, že sa jedná o globálnu premennú
-
-	console.log(a);
-
-	const myFunction = () => {
-		console.log(a);
-	}
-
-	myFunction();
-
-	console.log(a)
-	
-	//note	2.	Event listener
-	//!	-	pokiaľ ďalej nie je treba, treba ho odstrániť kvôli memory leaku
-	
-	const button = document.querySelector("button");
-	const onClick = () => {
-		console.log("Kliknutie")
-	}
-	button.addEventListener("click", onClick);
-
-	button.removeEventListener("click", onClick);
-
-	//note	3.	SetInterval
-	//!	-	pokiaľ nie je treba, taktiež treba vymazať pre memory leak
-	//		-	každý interval má svoje ID
-	let intervalID = setInterval(() => {
-		console.log("Test");
-	}, 3000);
-
-	console.log(intervalID);
-	clearInterval(intervalID);
+console.log(1);
+setTimeout( () => { 
+	console.log(2);
+}, 0)
+console.log(3);
